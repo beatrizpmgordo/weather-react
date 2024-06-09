74fe5a041b003ddb7efe6153e7e2069e51fc7fdb
beatrizpmgordo <beatriz.gordo@hotmail.com>
2024-06-09 21:23:50 +0100

    Added the axios integration so it shows the weather of a city

---
+++ src/Weather.js
@@ -0,0 +1,29 @@
+import React from "react";
+import axios from "axios";
+import { Puff } from "react-loader-spinner";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in ${response.data.city} is ${Math.round(
+        response.data.temperature.current
+      )}°C`
+    );
+  }
+
+  let apiKey = "f388b055e9fa4ta275o3155340404ccb";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+  return (
+    <Puff
+      visible={true}
+      height="80"
+      width="80"
+      color="#00BFFF"
+      ariaLabel="puff-loading"
+      wrapperStyle={{}}
+      wrapperClass=""
+    />
+  );
+}
